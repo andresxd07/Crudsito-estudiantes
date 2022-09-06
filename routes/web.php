ae739ac68b87fe5537410a33878d0a8636562e07
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2022-09-06 17:45:52 -0500

    Correciones finales

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,14 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::resource('student',StudentController::class)->middleware('auth');
+Route::resource('students',StudentController::class)->middleware('auth');
 
 Route::get('/home', [StudentController::class, 'index'])->name('home');
 
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+Route::delete('/students/delete/{id}', [StudentController::class, 'delete'])->name('students.delete');
+
 Route::group(['middleware' =>'auth'], function () {
-    Route::get('/', [StudentController::class, 'index'])->name('home');
+Route::get('/', [StudentController::class, 'index'])->name('index');
 
 });
