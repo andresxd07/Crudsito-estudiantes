63b92c2eed284f424e61383eb3cfb515278e6686
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2022-09-26 18:19:14 -0500

    Update RepositoriesServiceProvider.php

--- app/Providers/RepositoriesServiceProvider.php
+++ app/Providers/RepositoriesServiceProvider.php
@@ -3,15 +3,19 @@
 namespace App\Providers;
 
 
-use App\Repositories\StudentRepositoryInterface;
+use App\Repositories\Contracts\StudentRepositoryInterface;
+use App\Repositories\Contracts\SubjectRepositoryInterface;
 use App\Repositories\StudentRepository;
+use App\Repositories\SubjectRepository;
+
 use Illuminate\Support\ServiceProvider;
 
 class RepositoriesServiceProvider extends ServiceProvider {
 
     protected $classes = [
 
-        StudentRepositoryInterface::class => StudentRepository::class
+        StudentRepositoryInterface::class => StudentRepository::class,
+        SubjectRepositoryInterface::class => SubjectRepository::class
 
     ];
 
