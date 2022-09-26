7a1b3ff96571b293065fca60d298e2f8bbb19576
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2022-09-26 18:26:24 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Auth;
 
 
 Auth::routes();
 
 Route::resource('students',StudentController::class)->middleware('auth');
-
 Route::get('/home', [StudentController::class, 'index'])->name('home');
-
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+
 Route::delete('/students/delete/{id}', [StudentController::class, 'delete'])->name('students.delete');
 
 Route::group(['middleware' =>'auth'], function () {
 Route::get('/', [StudentController::class, 'index'])->name('index');
+
+Route::resource('subjects',SubjectController::class)->middleware('auth');
+Route::get('/home', [SubjectController::class, 'index'])->name('home');
+Route::get('/subjects/create', [SubjectController::class, 'create'])->name('subjects.create');
+
+Route::delete('/subjects/delete/{id}', [SubjectController::class, 'delete'])->name('subjects.delete');
+Route::get('/', [SubjectController::class, 'index'])->name('index');
+
 
 });
