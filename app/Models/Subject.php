85c9bcb8ab9d454ccf280c763242f44572d65573
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2022-10-08 14:08:26 -0500

    Update Subject.php

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -5,11 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *     @property int $id
+ *   * @property string $name_subject
+     * @property string $teacher
+     * @property string $hours
+     * @property string $days
+    
+ */
 class Subject extends Model
 {
     use HasFactory; 
     protected $table = 'subjects';
+    /**
+     * @var string[]
+     */
+
     protected $fillable = [
-        'NameSubject'
+        'name_subject'
     ];
+    /**
+     * @var string[]
+     */
 }
