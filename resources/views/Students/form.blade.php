bc03c23f9e73c67fd030c41be031ff0eba945473
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2022-10-08 15:29:36 -0500

    Update form.blade.php

--- resources/views/Students/form.blade.php
+++ resources/views/Students/form.blade.php
@@ -1,7 +1,5 @@
 
-
 <h1> {{$modo}} Estudiante </h1>
-
 
 @if(count($errors)>0)
 <div class="alert alert-danger" role="alert">
@@ -9,50 +7,43 @@
       @foreach($errors->all() as $error)
         <li> {{ $error }} </li>
        @endforeach
-
-
-
     </ul>
-
 </div>
-
-
-    
 
 @endif
 
 <div class="form-group">
 
-<label for="Name"> Nombre </label>
-<input type="text" class="form-control" name="Name" value="{{ isset($student->Name)?$student->Name:old('Name')}}" id="Name"> 
+<label for="name"> Nombre </label>
+<input type="text" class="form-control" name="name" value="{{ isset($student->name)?$student->name:old('name')}}" id="name"> 
 </div>
 
 <div class="form-group">
-<label for="Firstlastname"> Primer Apellido </label>
-<input type="text" class="form-control" name="Firstlastname" value="{{ isset($student->Firstlastname)?$student->Firstlastname:old('Firstlastname') }}" id="Firstlastname">  
+<label for="first_last_name"> Primer Apellido </label>
+<input type="text" class="form-control" name="first_last_name" value="{{ isset($student->first_last_name)?$student->first_last_name:old('first_last_name') }}" id="first_last_name">  
 </div>
 
 <div class="form-group">
-<label for="Secondlastname"> Segundo Apellido </label>
-<input type="text" class="form-control" name="Secondlastname" value="{{ isset($student->Secondlastname)?$student->Secondlastname:old('Secondlastname') }}" id="Secondlastname">   
+<label for="second_last_name"> Segundo Apellido </label>
+<input type="text" class="form-control" name="second_last_name" value="{{ isset($student->second_last_name)?$student->second_last_name:old('second_last_name') }}" id="second_last_name">   
 </div>
 <div class="form-group">
-<label for="Mail"> Correo </label>
-<input type="email" class="form-control" name="Mail" value="{{ isset($student->Mail)?$student->Mail:old('Mail') }}" id="Mail">  
+<label for="mail"> Correo </label>
+<input type="email" class="form-control" name="mail" value="{{ isset($student->mail)?$student->mail:old('mail') }}" id="mail">  
 </div>
 
 <div class="form-group">
-<label for="Photo">  </label>
+<label for="photo">  </label>
 
-@if(isset($student->Photo))
-<img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$student->Photo }}" width="100" alt="">
+@if(isset($student->photo))
+<img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$student->photo }}" width="100" alt="">
 @endif
-<input type="file" class="form-control" name="Photo" value="" id="Photo">  
+<input type="file" class="form-control" name="photo" value="" id="photo">  
 </div>
 
 <div class="form-group">
-<label for="Course"> Curso </label>
-<select name="Course" class="form-control" value="{{ isset($student->Course)?$student->Course:old('Course') }}" id="Course">  
+<label for="course"> Curso </label>
+<select name="course" class="form-control" value="{{ isset($student->course)?$student->course:old('course') }}" id="course">  
   <option disabled=""> Selecciona una opcion</option>
 <option value="Primero"> Primero</option>
 <option value="Segundo"> Segundo</option>
@@ -66,17 +57,13 @@
 <option value="Decimo"> Decimo</option>
 <option value="Once"> Once</option>
 
-
-
 </select>
-
-
 
 </div>
 
 <div class="form-group">
-<label for="Gender"> Genero </label>
-<select class="form-control" name="Gender" value="{{ isset($student->Gender)?$student->Gender:old('Gender') }}" id="Gender">  
+<label for="gender"> Genero </label>
+<select class="form-control" name="gender" value="{{ isset($student->gender)?$student->gender:old('gender') }}" id="gender">  
 <option disabled=""> Selecciona una opcion</option>
 <option value="Masculino"> Masculino</option>
 <option value="Femenino"> Femenino</option>
@@ -86,15 +73,10 @@
 
 </div>
 <div class="form-group">
-  <label for="School"> Colegio </label>
-  <input type="name" class="form-control" name="School" value="{{ isset($student->School)?$student->School:old('School') }}" id="School">  
+  <label for="school"> Colegio </label>
+  <input type="name" class="form-control" name="school" value="{{ isset($student->school)?$student->school:old('school') }}" id="school">  
   </div>
-
-  
 
 <input type="submit" class="btn btn-success" value="{{ $modo }} datos">  
 
 <a  class="btn btn-secondary" href="{{ url('students/')}}">Regresar</a>
-<br>
-
-
